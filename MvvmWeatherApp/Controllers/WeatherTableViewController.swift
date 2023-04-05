7254857d9a8fc92f7e2caa5f5666d3e23e72d99b
Zülfü Akgüneş <52940048+zulfuakgunes@users.noreply.github.com>
2023-04-05 15:06:13 +0300

    Custom cell
    Displaying weather hard-coded into cell

---
+++ MvvmWeatherApp/Controllers/WeatherTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherTableViewController.swift
+//  MvvmWeatherApp
+//
+//  Created by Zülfü Akgüneş on 5.04.2023.
+//
+
+import Foundation
+import UIKit
+
+class WeatherTableViewController: UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        cell.cityNameLabel.text = "Houston"
+        cell.degreeLabel.text = "70"
+        
+        
+        return cell
+    }
+
+}
---
+++ MvvmWeatherApp/CustomCells/WeatherCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherCell.swift
+//  MvvmWeatherApp
+//
+//  Created by Zülfü Akgüneş on 5.04.2023.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell{
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var degreeLabel: UILabel!
+}
